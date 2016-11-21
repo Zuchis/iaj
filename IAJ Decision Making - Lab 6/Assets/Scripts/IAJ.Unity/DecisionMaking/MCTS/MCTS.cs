b6339735679bf176a5be735c69a9d3c5dd524a46
luiscalmeida <l_birdhouse@hotmail.com>
2016-11-21 13:18:26 +0000

    lab 6 folder added

---
+++ IAJ Decision Making - Lab 6/Assets/Scripts/IAJ.Unity/DecisionMaking/MCTS/MCTS.cs
@@ -0,0 +1,140 @@
+﻿using Assets.Scripts.IAJ.Unity.DecisionMaking.GOB;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.IAJ.Unity.DecisionMaking.MCTS
+{
+    public class MCTS
+    {
+        public const float C = 1.4f;
+        public bool InProgress { get; private set; }
+        public int MaxIterations { get; set; }
+        public int MaxIterationsProcessedPerFrame { get; set; }
+        public int MaxPlayoutDepthReached { get; private set; }
+        public int MaxSelectionDepthReached { get; private set; }
+        public float TotalProcessingTime { get; private set; }
+        public MCTSNode BestFirstChild { get; set; }
+        public List<GOB.Action> BestActionSequence { get; private set; }
+
+
+        private int CurrentIterations { get; set; }
+        private int CurrentIterationsInFrame { get; set; }
+        private int CurrentDepth { get; set; }
+
+        private CurrentStateWorldModel CurrentStateWorldModel { get; set; }
+        private MCTSNode InitialNode { get; set; }
+        private System.Random RandomGenerator { get; set; }
+        
+        
+
+        public MCTS(CurrentStateWorldModel currentStateWorldModel)
+        {
+            this.InProgress = false;
+            this.CurrentStateWorldModel = currentStateWorldModel;
+            this.MaxIterations = 100;
+            this.MaxIterationsProcessedPerFrame = 10;
+            this.RandomGenerator = new System.Random();
+        }
+
+
+        public void InitializeMCTSearch()
+        {
+            this.MaxPlayoutDepthReached = 0;
+            this.MaxSelectionDepthReached = 0;
+            this.CurrentIterations = 0;
+            this.CurrentIterationsInFrame = 0;
+            this.TotalProcessingTime = 0.0f;
+            this.CurrentStateWorldModel.Initialize();
+            this.InitialNode = new MCTSNode(this.CurrentStateWorldModel)
+            {
+                Action = null,
+                Parent = null,
+                PlayerID = 0
+            };
+            this.InProgress = true;
+            this.BestFirstChild = null;
+            this.BestActionSequence = new List<GOB.Action>();
+        }
+
+        public GOB.Action Run()
+        {
+            MCTSNode selectedNode;
+            Reward reward;
+
+            var startTime = Time.realtimeSinceStartup;
+            this.CurrentIterationsInFrame = 0;
+
+            
+        }
+
+        private MCTSNode Selection(MCTSNode initialNode)
+        {
+            GOB.Action nextAction;
+            MCTSNode currentNode = initialNode;
+            MCTSNode bestChild;
+                      
+            while (!currentNode.State.IsTerminal())
+            {
+                GOB.Action[] validMoves = currentNode.State.GetExecutableActions();
+
+                if(validMoves.Length > currentNode.ChildNodes.Count)  // currentNode not fully expanded
+                {
+                    return Expand(currentNode, validMoves[0]);  // not validMoves[0]
+                }
+                else
+                {
+                    currentNode = ;    // currentNode = currentNode.bestchild;
+                }
+            }
+
+            //TODO: implement
+            throw new NotImplementedException();
+        }
+
+
+
+
+        private Reward Playout(WorldModel initialPlayoutState)
+        {
+            while (!initialPlayoutState.IsTerminal())
+            {
+                GOB.Action a = initialPlayoutState.GetNextAction();
+                a.ApplyActionEffects(initialPlayoutState);
+            }
+            Reward r = new Reward();
+            r.Value = initialPlayoutState.GetScore();   //??????????
+            return new Reward();
+
+            //Random rand = new Random((int)DateTime.Now.Ticks);      
+        }
+
+        private void Backpropagate(MCTSNode node, Reward reward)
+        {
+            while (node != null)
+            {
+                node.N += 1;
+                node.Q += 1; // instead of 1 : reward(node, Player(Parent(node)))
+                node = node.Parent;
+           } 
+        }
+
+        private MCTSNode Expand(MCTSNode parent, GOB.Action action)
+        {
+           
+        }
+
+        //gets the best child of a node, using the UCT formula
+        private MCTSNode BestUCTChild(MCTSNode node)
+        {
+            
+        }
+
+        //this method is very similar to the bestUCTChild, but it is used to return the final action of the MCTS search, and so we do not care about
+        //the exploration factor
+        private MCTSNode BestChild(MCTSNode node)
+        {
+           
+        }
+    }
+}
